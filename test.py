18f04d1fea633207d6e33353668101ffd1c26876
Usman Khwaja <numbfall@live.com>
2015-05-16 17:58:16 +0800

    bug fixes and relevant tweets

--- test.py
+++ test.py
@@ -4,19 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 
-# Path to the data file
-tweets_data_path = 'twitter_data.txt'
-
-# Read the file
-tweets_data = []
-tweets_file = open(tweets_data_path, "r")
-for line in tweets_file:
-    try:
-        tweet = json.loads(line)
-        tweets_data.append(tweet)
-    except:
-        continue
-
 # Function to test presence of word in a tweet
 def word_in_text(word, text):
     word = word.lower()
@@ -26,35 +13,82 @@ def word_in_text(word, text):
         return True
     return False
 
-# Create a data frame
-tweets = pd.DataFrame()
+def main():
+	# Path to the data file
+	tweets_data_path = 'twitter_data.txt'
 
-# Add text column to data frame
-tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+	# Read the file
+	tweets_data = []
+	tweets_file = open(tweets_data_path, "r")
+	for line in tweets_file:
+		try:
+			tweet = json.loads(line)
+			tweets_data.append(tweet)
+		except:
+			continue
+	
+	print len(tweets_data)   
 
-# Three columns fot the language into the data frame
-tweets['python'] = tweets['text'].apply(lambda tweet: word_in_text('python', tweet))
-tweets['javascript'] = tweets['text'].apply(lambda tweet: word_in_text('javascript', tweet))
-tweets['ruby'] = tweets['text'].apply(lambda tweet: word_in_text('ruby', tweet))
+	# Create a data frame
+	tweets = pd.DataFrame()
+
+	# Add text column to data frame
+	tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+
+	# Three columns fot the language into the data frame
+	tweets['python'] = tweets['text'].apply(lambda tweet: word_in_text('python', tweet))
+	tweets['javascript'] = tweets['text'].apply(lambda tweet: word_in_text('javascript', tweet))
+	tweets['ruby'] = tweets['text'].apply(lambda tweet: word_in_text('ruby', tweet))
 
 
-print tweets['python'].value_counts()[True]
-print tweets['javascript'].value_counts()[True]
-print tweets['ruby'].value_counts()[True]
+	print tweets['python'].value_counts()[True]
+	print tweets['javascript'].value_counts()[True]
+	print tweets['ruby'].value_counts()[True]
 
-# Making the Comparison Chart
-prg_langs = ['python', 'javascript', 'ruby']
-tweets_by_prg_lang = [tweets['python'].value_counts()[True], tweets['javascript'].value_counts()[True], tweets['ruby'].value_counts()[True]]
+	# Making the Comparison Chart
+	prg_langs = ['python', 'javascript', 'ruby']
+	tweets_by_prg_lang = [tweets['python'].value_counts()[True], tweets['javascript'].value_counts()[True], tweets['ruby'].value_counts()[True]]
 
-x_pos = list(range(len(prg_langs)))
-width = 0.8
-fig, ax = plt.subplots()
-plt.bar(x_pos, tweets_by_prg_lang, width, alpha=1, color='g')
+	x_pos = list(range(len(prg_langs)))
+	width = 0.8
+	fig, ax = plt.subplots()
+	plt.bar(x_pos, tweets_by_prg_lang, width, alpha=1, color='g')
 
-# Setting axis labels and ticks
-ax.set_ylabel('Number of tweets', fontsize=15)
-ax.set_title('Ranking: python vs. javascript vs. ruby (Raw data)', fontsize=10, fontweight='bold')
-ax.set_xticks([p + 0.4 * width for p in x_pos])
-ax.set_xticklabels(prg_langs)
-plt.grid()
-plt.savefig('tweet_by_prg_language_1', format='png')
+	# Setting axis labels and ticks
+	ax.set_ylabel('Number of tweets', fontsize=15)
+	ax.set_title('Ranking: python vs. javascript vs. ruby (Raw data)', fontsize=10, fontweight='bold')
+	ax.set_xticks([p + 0.4 * width for p in x_pos])
+	ax.set_xticklabels(prg_langs)
+	plt.grid()
+	plt.savefig('tweet_by_prg_language_1', format='png')
+
+	# Targeting relevant tweets
+	tweets['programming'] = tweets['text'].apply(lambda tweet: word_in_text('programming', tweet))
+	tweets['code'] = tweets['text'].apply(lambda tweet: word_in_text('code', tweet))
+	tweets['relevant'] = tweets['text'].apply(lambda tweet: word_in_text('programming', tweet) or word_in_text('code', tweet))
+
+	print tweets['programming'].value_counts()[True]
+	print tweets['code'].value_counts()[True]
+	print tweets['relevant'].value_counts()[True]
+
+	print tweets[tweets['relevant'] == True]['python'].value_counts()[True]
+	print tweets[tweets['relevant'] == True]['javascript'].value_counts()[True]
+	print tweets[tweets['relevant'] == True]['ruby'].value_counts()[True]
+
+	tweets_by_prg_lang = [tweets[tweets['relevant'] == True]['python'].value_counts()[True], 
+                      tweets[tweets['relevant'] == True]['javascript'].value_counts()[True], 
+                      tweets[tweets['relevant'] == True]['ruby'].value_counts()[True]]
+	x_pos = list(range(len(prg_langs)))
+	width = 0.8
+	fig, ax = plt.subplots()
+	plt.bar(x_pos, tweets_by_prg_lang, width,alpha=1,color='g')
+	ax.set_ylabel('Number of tweets', fontsize=15)
+	ax.set_title('Ranking: python vs. javascript vs. ruby (Relevant data)', fontsize=10, fontweight='bold')
+	ax.set_xticks([p + 0.4 * width for p in x_pos])
+	ax.set_xticklabels(prg_langs)
+	plt.grid()
+	plt.savefig('tweet_by_prg_language_2', format='png')
+
+
+if __name__=='__main__':
+	main()
